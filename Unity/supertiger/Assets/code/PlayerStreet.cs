d8e2c6773f809f73b65915ec54983897966119b0
kalin.eh <kalin.eh@gmail.com>
2016-11-16 00:00:49 -0800

    input rejig

--- Unity/supertiger/Assets/code/PlayerStreet.cs
+++ Unity/supertiger/Assets/code/PlayerStreet.cs
@@ -6,135 +6,348 @@ public class PlayerStreet
     : MonoBehaviour
 {
     public Collider2D FloorCollider;
-    private Collider2D collider;
+    private Collider2D selfCollider;
 
-    private Vector2 acc;
     private Vector2 vel;
     private Rigidbody2D body;
+
+    private float forceX;
+    private float forceY;
+
+    private int attackFrames;
+    private int jumpFrames;
 
     public enum BodyState
     {
         Idle,
-        Step,
-        Run,
-        Reverse,
+        Land,
+        Walk,
+        PunchCharge,
+        Punch,
+        JumpPushing,
+        JumpRising,
+        JumpFalling,
     }
 
     private BodyState bodyState;
     private float bodyFacing;
 
-    public void Start()
-    {
-        body = GetComponent<Rigidbody2D>();
-        collider = GetComponent<Collider2D>();
-    }
-
     public void Update()
     {
-        UpdateInput();
-        UpdateBodyState();
-        UpdateBodyStateVisual();
     }
 
-    public void UpdateBodyState()
+    public void OnEnable()
     {
-        var vx = vel.x;
-        var vy = vel.y;
-        var ax = acc.x;
-        var ay = acc.y;
-        var vxa = Mathf.Abs(vx);
-        var vya = Mathf.Abs(vy);
-        var axa = Mathf.Abs(ax);
-        var aya = Mathf.Abs(ay);
-        var vxs = Mathf.Sign(vx);
-        var vys = Mathf.Sign(vy);
-        var axs = Mathf.Sign(ax);
-        var ays = Mathf.Sign(ay);
+        body = GetComponent<Rigidbody2D>();
+        selfCollider = GetComponent<Collider2D>();
 
-        if (vxa > 0.10f)
-        {
-            bodyState = BodyState.Run;
-            bodyFacing = vxs;
-            return;
-        }
-
-        if (vxa > 0.05f)
-        {
-            bodyState = BodyState.Step;
-            bodyFacing = vxs;
-            return;
-        }
-
-        bodyState = BodyState.Idle;
-        bodyFacing = vxs;
+        StartCoroutine(DoUpdateInput());
+        //StartCoroutine(DoUpdateBodyState());
+        //StartCoroutine(DoUpdateBodyStateVisual());
     }
 
-    public void UpdateBodyStateVisual()
+    public bool IsJumpingAny()
+    {
+        return
+            bodyState == BodyState.JumpPushing ||
+            bodyState == BodyState.JumpRising ||
+            bodyState == BodyState.JumpFalling;
+    }
+
+    public bool IsTouchingFloor()
+    {
+        return Physics2D.IsTouching(selfCollider, FloorCollider);
+    }
+
+    /*
+    public IEnumerator DoUpdateBodyState()
+    {
+        yield break;
+        var frameCycle = new WaitForFixedUpdate();
+
+        while (true)
+        {
+            var vx = vel.x;
+            var vy = vel.y;
+            var vxa = Mathf.Abs(vx);
+            var vya = Mathf.Abs(vy);
+            var vxs = Mathf.Sign(vx);
+            var vys = Mathf.Sign(vy);
+
+            if (IsJumpingAny())
+            {
+                var landed = IsTouchingFloor();
+                if (landed)
+                {
+                    bodyState = BodyState.Idle;
+                    yield return new WaitForSeconds(0.1f);
+                    continue;
+                }
+
+                bodyFacing = vxs;
+                yield return null;
+                continue;
+            }
+
+            if (vxa > 0.10f)
+            {
+                //bodyState = BodyState.Run;
+                bodyFacing = vxs;
+
+                yield return null;
+                continue;
+            }
+
+            if (vxa > 0.05f)
+            {
+                //bodyState = BodyState.Step;
+                bodyFacing = vxs;
+
+                yield return null;
+                continue;
+            }
+
+            bodyState = BodyState.Idle;
+            bodyFacing = vxs;
+        }
+    }
+    */
+
+        /*
+    public IEnumerator DoUpdateBodyStateVisual()
     {
         var body = transform.Find("Visual/Body").GetComponent<Renderer>().material;
         var feet = transform.Find("Visual/Feet").GetComponent<Renderer>().material;
 
-        switch (bodyState)
+        while (true)
         {
-            case BodyState.Idle: body.color = Color.white; break;
-            case BodyState.Step: body.color = Color.Lerp(Color.white, Color.green, 0.25f); break;
-            case BodyState.Run: body.color = Color.Lerp(Color.white, Color.green, 1.0f); break;
-            case BodyState.Reverse: body.color = Color.Lerp(Color.white, Color.green, 0.5f); break;
+            switch (bodyState)
+            {
+                case BodyState.Idle: body.color = Color.white; break;
+                //case BodyState.Step: body.color = Color.Lerp(Color.white, Color.green, 0.25f); break;
+                //case BodyState.Run: body.color = Color.Lerp(Color.white, Color.green, 1.0f); break;
+                //case BodyState.Reverse: body.color = Color.Lerp(Color.white, Color.green, 0.5f); break;
+                //case BodyState.Jump: body.color = Color.Lerp(Color.white, Color.red, 0.5f); break;
+            }
+
+            var landed = Physics2D.IsTouching(selfCollider, FloorCollider);
+
+            feet.color = landed ? Color.blue : Color.white;
+
+            yield return null;
         }
-
-        var landed = Physics2D.IsTouching(collider, FloorCollider);
-
-        feet.color = landed ? Color.blue : Color.white;
     }
+    */
 
     public void FixedUpdate()
     {
-        UpdateMovement();
     }
 
-    public void UpdateInput()
+    public IEnumerator DoUpdateInput()
     {
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
+        while (true)
+        {
+            var inputX = Input.GetAxis("Horizontal");
+            var inputY = Input.GetAxis("Vertical");
 
-        var punch = Input.GetButtonDown("Fire1");
-        var kick = Input.GetButtonDown("Fire2");
-        var jump = Input.GetButtonDown("Fire3");
+            var inputUp = inputY > 0.0f;
+            var inputDown = inputY < 0.0f;
 
-        var dt = Time.deltaTime;
+            if (Input.GetButton("Fire1"))
+                attackFrames++;
+            else
+                attackFrames = 0;
 
-        acc += Vector2.right * x * dt;
-        acc += Vector2.up * y * dt;
+            if (Input.GetButton("Jump"))
+                jumpFrames++;
+            else
+                jumpFrames = 0;
+
+            var landed = IsTouchingFloor();
+            var dt = Time.deltaTime;
+
+            switch (bodyState)
+            {
+                case BodyState.Idle:
+                    {
+                        if (jumpFrames == 1)
+                        {
+                            bodyState = BodyState.JumpPushing;
+                            forceY = 0.3f;
+
+                            EjectFromFloor();
+
+                            break;
+                        }
+
+                        if (attackFrames == 1)
+                        {
+                            bodyState = BodyState.PunchCharge;
+                            forceX = 0.0f;
+                            forceY = 0.0f;
+                            jumpFrames = 0;
+                            break;
+                        }
+
+                        forceX += inputX * 0.05f;
+                        break;
+                    }
+                case BodyState.Land:
+                    {
+                        bodyState = BodyState.Idle;
+                        break;
+                    }
+                case BodyState.Walk:
+                    {
+                        bodyState = BodyState.Idle;
+                        break;
+                    }
+                case BodyState.PunchCharge:
+                    {
+                        bodyState = BodyState.Idle;
+                        break;
+                    }
+                case BodyState.JumpPushing:
+                    {
+                        forceX += inputX * 0.05f;
+                        forceY += 0.006f;
+                        forceY -= (float)jumpFrames * 0.0003f;
+                        forceY = Mathf.Max(forceY, 0.0f);
+
+                        if (jumpFrames == 0 || forceY <= 0.0f)
+                        {
+                            bodyState = BodyState.JumpRising;
+                            forceY = 0.0f;
+                            break;
+                        }
+
+                        break;
+                    }
+                case BodyState.JumpRising:
+                    {
+                        forceX += inputX * 0.04f;
+                        forceY -= (float)jumpFrames * 0.0002f;
+                        forceY = Mathf.Max(forceY, 0.0f);
+
+                        if (forceY <= 0.0f)
+                        {
+                            bodyState = BodyState.JumpFalling;
+                            break;
+                        }
+
+                        break;
+                    }
+                case BodyState.JumpFalling:
+                    {
+                        forceX += inputX * 0.04f;
+                        forceY -= 0.011f;
+
+                        if (landed)
+                        {
+                            bodyState = BodyState.Land;
+                            EjectFromFloor();
+                            break;
+                        }
+
+                        break;
+                    }
+            }
+
+            vel += new Vector2(forceX, forceY);
+
+            if (landed)
+                vel.y = 0.0f;
+
+            body.position += vel;
+
+            forceX = Mathf.Clamp(forceX, -0.03f, 0.03f);
+            forceY = Mathf.Clamp(forceY, -0.25f, 0.25f);
+            vel.x = Mathf.Clamp(vel.x, -0.03f, 0.03f);
+            vel.y = Mathf.Clamp(vel.y, -0.05f, 0.05f);
+
+            if (landed)
+                vel.x *= 0.8f;
+            else
+                vel.x *= 0.8f;
+
+            vel.y *= 0.8f;
+
+            forceX *= 0.40f;
+            forceY *= 0.92f;
+
+            if (Mathf.Abs(forceX) < 0.001f) forceX = 0.0f;
+            if (Mathf.Abs(forceY) < 0.001f) forceY = 0.0f;
+            if (Mathf.Abs(vel.x) < 0.001f) vel.x = 0.0f;
+            if (Mathf.Abs(vel.y) < 0.001f) vel.y = 0.0f;
+
+            yield return new WaitForFixedUpdate();
+        }
     }
 
+    public void EjectFromFloor()
+    {
+        for (int i = 0; i < 32; ++i)
+        {
+            body.position += Vector2.up * 0.01f;
+            transform.position += Vector3.up * 0.01f;
+
+            if (!IsTouchingFloor())
+                break;
+        }
+    }
+
+    public void EjectFromFloorRay()
+    {
+        var height = selfCollider.bounds.size.y * 0.5f;
+        var mask = LayerMask.GetMask("Floor");
+        var hit = Physics2D.Raycast(transform.position, Vector2.down, height + 0.25f, mask);
+        if (!hit)
+            return;
+
+        var dist = hit.distance;
+
+        body.position += Vector2.up * (height - dist);
+    }
+
+    /*
     public void UpdateMovement()
     {
-        var landed = Physics2D.IsTouching(collider, FloorCollider);
+        var landed = Physics2D.IsTouching(selfCollider, FloorCollider);
 
         var maxSpeedX = 0.3f;
         var frictionAirX = 0.20f;
         var frictionFloorX = 0.70f;
-        var controlAirX = 15.0f;
+        var controlAirX = 85.0f;
         var controlFloorX = 15.0f;
 
         var controlX = landed ? controlFloorX : controlAirX;
         var frictionX = landed ? frictionFloorX : frictionAirX;
 
-        vel.x += acc.x * controlX;
+        //vel.x += acc.x * controlX;
         vel.x = Mathf.Clamp(vel.x, -maxSpeedX, maxSpeedX);
         vel.x *= frictionX;
 
+        var maxSpeedY = 0.2f;
+        var jumpForceY = 95.0f;
+        var gravityForceY = 0.0f;
+        var frictionY = 0.8f;
+
+        //vel.y += acc.y * jumpForceY;
+        vel.y = Mathf.Clamp(vel.y, -maxSpeedY, maxSpeedY);
+        vel.y *= frictionY;
+
         //Debug.LogFormat("Vel.x: {0}, acc: {0}", vel.x, acc.x);
 
-        acc.x = 0.0f;
-        acc.y = 0.0f;
+        //acc.x = 0.0f;
+        //acc.y = 0.0f;
 
-        if (Mathf.Abs(vel.x) <= 0.01f) vel.x = 0.0f;
-        if (Mathf.Abs(vel.y) <= 0.01f) vel.y = 0.0f;
-        if (Mathf.Abs(acc.x) <= 0.01f) acc.x = 0.0f;
-        if (Mathf.Abs(acc.y) <= 0.01f) acc.y = 0.0f;
-
+        //if (Mathf.Abs(vel.x) <= 0.01f) vel.x = 0.0f;
+        //if (Mathf.Abs(vel.y) <= 0.01f) vel.y = 0.0f;
+        //if (Mathf.Abs(acc.x) <= 0.01f) acc.x = 0.0f;
+        //if (Mathf.Abs(acc.y) <= 0.01f) acc.y = 0.0f;
+//
         body.position += vel;
         body.velocity = Vector2.zero;
     }
+    */
 }
